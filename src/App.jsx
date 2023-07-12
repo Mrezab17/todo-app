fa16e1a46c1bdba07a92e4d04bd80057a23d1146
mreza <mrezabmiri17@gmail.com>
2023-07-12 17:39:55 +0330

    Header Component Added instead of normal h3 tag .

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
       <div className="app_wrapper col-12">
-        <div className="row-1">
-          <div className="col-6">
-            <h1>Todo App</h1>
-          </div>
-        </div>
+        <Header />
       </div>
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { StyledRow } from "./styled/Row.styled";
+const Header = () => {
+  return (
+    <StyledRow>
+      <h1>Todo App</h1>
+    </StyledRow>
+  );
+};
+
+export default Header;
