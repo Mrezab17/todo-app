a21dadc40a4a857a4e797dd327bc1478d4c96159
mreza <mrezabmiri17@gmail.com>
2023-07-12 16:11:40 +0330

    app internal wrapper completed .

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="row app"></div>;
+  return (
+    <div className="row app">
+      <div className="app_wrapper col-12"></div>
+    </div>
+  );
 }
 
 export default App;
