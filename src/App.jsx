774d8ab9db709590927d36d3fdc4ea7506010160
mreza <mrezabmiri17@gmail.com>
2023-07-12 16:30:08 +0330

    Header Added.

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="row app">
-      <div className="app_wrapper col-12"></div>
+    <div className="app">
+      <div className="app_wrapper col-12">
+        <div className="row-1">
+          <div className="col-6">
+            <h1>Todo App</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
