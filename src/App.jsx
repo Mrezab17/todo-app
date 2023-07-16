5f44a285210b4a2f72e81c73e80770069a17bbf4
mreza <mrezabmiri17@gmail.com>
2023-07-16 15:29:02 +0330

    Router and Switch Added .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom/cjs/react-router-dom.min";
+
 const App = () => {
-  return <></>;
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
