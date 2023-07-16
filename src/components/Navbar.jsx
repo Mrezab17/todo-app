339a660d411dfd39e081062fb4cce21c42f170f8
mreza <mrezabmiri17@gmail.com>
2023-07-16 15:55:15 +0330

    Navbar Completed.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,20 @@
+import { Link } from "react-router-dom/cjs/react-router-dom";
 const Navbar = () => {
   return (
-    <div className="bg-gray-800 mx-auto max-w-7xl px-2 sm:px-6 lg:px-8"></div>
+    <div className="bg-gray-800 w-full h-16 pl-10 space-x-10 items-center flex fixed">
+      <Link
+        to="/"
+        className="w-1/12 text-xl text-white hover:text-purple-400 hover:text-3xl"
+      >
+        Home
+      </Link>
+      <Link
+        className="w-1/12 text-xl text-white hover:text-purple-400 hover:text-3xl"
+        to="/register"
+      >
+        Register
+      </Link>
+    </div>
   );
 };
 
