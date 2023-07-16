57cf6b81a739c33255f44fd8c9ec16aaabf2a250
mreza <mrezabmiri17@gmail.com>
2023-07-16 13:20:57 +0330

    Task Monitor Updated . Unnecessary imports and components deleted .

--- src/components/TaskMonitor.jsx
+++ src/components/TaskMonitor.jsx
@@ -1,15 +1,16 @@
-import { StyledRow } from "./styled/Row.styled";
-import { StyledLabel } from "./styled/Label.styled";
-import { StyledButtonDelete } from "./styled/ButtonDelete.styled";
-
 const TaskMonitor = (props) => {
   return (
-    <StyledRow>
-      <StyledLabel>You Have {props.number} Pending tasks</StyledLabel>
-      <StyledButtonDelete onClick={props.onRemoveAll}>
+    <div className="w-full h-12 items-center flex space-x-0.5 text-xl">
+      <label className="bg-white w-8/12 h-full pl-5 items-center flex ">
+        You Have {props.number} Pending tasks
+      </label>
+      <button
+        className="bg-purple-700/100 text-white w-24 h-full items-center justify-center text-xl rounded"
+        onClick={props.onRemoveAll}
+      >
         Clear All
-      </StyledButtonDelete>
-    </StyledRow>
+      </button>
+    </div>
   );
 };
 
--- src/components/styled/Row.styled.jsx
+++
@@ -1,7 +0,0 @@
-import { styled } from "styled-components";
-
-export const StyledRow = styled.div`
-  width: 100%;
-  height: 10%;
-  margin-top: 5px;
-`;
